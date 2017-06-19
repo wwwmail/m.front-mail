d08fd6c381f91c75e50b3ed383aa4b0cbe4e495e
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-19 15:47:31 +0300

    Added build

---
+++ src/client/app/directives/scroll-left.directive.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives')
+        .directive('scrollLeft', scrollLeft);
+
+    scrollLeft.$inject = ['CONFIG'];
+
+    /* @ngInject */
+    function scrollLeft(CONFIG) {
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            // $(element).scrollLeft($(this).height());
+
+            $(document).ready(function () {
+                $('.menu-main-layout__user-info-container').scrollLeft($(this).height());
+            });
+        }
+    }
+
+})();
