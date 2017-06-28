a199522c007ddae624d1a607613b8c35e1c47eae
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-28 21:47:16 +0300

    Added build

---
+++ src/client/app/directives/body-message-scroll.directive.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives')
+        .directive('bodyMessageScroll', bodyMessageScroll);
+
+    bodyMessageScroll.$inject = ['$timeout'];
+
+    /* @ngInject */
+    function bodyMessageScroll($timeout) {
+        var directive = {
+            link: link,
+            restrict: 'A',
+            scope: true
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            $timeout(function () {
+                // console.log('el', $(element).height());
+                $(element).css('height', $(element).height());
+            }, 250);
+        }
+    }
+
+})();
