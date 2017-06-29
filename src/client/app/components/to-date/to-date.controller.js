59ef268b4d058cd61f195c505c7fb144a8143554
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-30 00:23:15 +0300

    Added build

--- src/client/app/components/to-date/to-date.controller.js
+++ src/client/app/components/to-date/to-date.controller.js
@@ -5,35 +5,45 @@
         .module('app.components')
         .controller('ToDateController', ToDateController);
 
-    ToDateController.$inject = ['$scope'];
+    ToDateController.$inject = ['$scope', '$translate'];
     /* @ngInject */
-    function ToDateController($scope) {
+    function ToDateController($scope, $translate) {
         var vm = this;
 
         vm.convertDate = '';
 
         vm.calendarFormat = {
-            sameDay: 'HH:mm',
-            nextDay: '[завтра]',
-            nextWeek: 'dddd',
-            lastDay: '[вчера] hh:mm',
-            lastWeek: 'DD [го] MMMM YYYY [в] hh:mm',
-            sameElse: 'DD [го] MMMM YYYY [в] hh:mm'
+            // sameDay: 'HH:mm',
+            // nextDay: '[завтра]',
+            // nextWeek: 'dddd HH:mm',
+            // lastDay: '[вчера] hh:mm',
+            // lastWeek: 'DD MMMM YYYY [в] hh:mm',
+            // sameElse: 'DD MMMM YYYY [в] hh:mm'
         };
 
         vm.calendarSmallFormat = {
-            sameDay: 'HH:mm',
-            nextDay: '[Tomorrow]',
-            nextWeek: 'dddd',
-            lastDay: 'D MMM',
-            lastWeek: 'D MMM YY',
-            sameElse: 'D MMM YY'
+            // sameDay: 'HH:mm',
+            // nextDay: '[завтра] HH:mm',
+            // nextWeek: 'dddd HH:mm',
+            // lastDay: 'D MMM',
+            // lastWeek: 'D MMM',
+            // sameElse: 'D MMM'
+        };
+
+        vm.sendTimeFormat = {
+            // sameDay: '[сегодня в] HH:mm',
+            // nextDay: '[завтра] HH:mm',
+            // nextWeek: 'dddd HH:mm',
+            // lastDay: '[вчера] hh:mm',
+            // lastWeek: 'DD MMMM YYYY [в] hh:mm',
+            // sameElse: 'DD MMMM YYYY [в] hh:mm'
         };
 
         vm.getConvert = getConvert;
 
         $scope.$watch('vm.date', function (data, newData) {
             if (data) {
+                console.log('data', data);
                 var newDate = moment(data);
                 vm.convertDate = getConvert(newDate);
             }
@@ -48,21 +58,192 @@
         activate();
 
         function activate() {
-            // moment().format('LLL');  // 11 мая 2017 г., 22:14
+            if (vm.isSmall) {
+                moment.locale('ru', {
+                    calendar: {
+                        sameDay: 'HH:mm',
+                        lastDay: 'D MMM',
+                        nextDay: '[завтра]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'D MMM',
+                        sameElse: 'D MMM'
+                    }
+                });
+
+                moment.locale('en', {
+                    calendar: {
+                        sameDay: 'HH:mm',
+                        lastDay: 'D MMM',
+                        nextDay: '[tomorrow at]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'D MMM',
+                        sameElse: 'D MMM'
+                    }
+                });
+
+                moment.locale('cs', {
+                    calendar: {
+                        sameDay: 'HH:mm',
+                        lastDay: 'D MMM',
+                        nextDay: '[zítra]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'D MMM',
+                        sameElse: 'D MMM'
+                    }
+                });
+
+                moment.locale('sk', {
+                    calendar: {
+                        sameDay: 'HH:mm',
+                        lastDay: 'D MMM',
+                        nextDay: '[zítra]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'D MMM',
+                        sameElse: 'D MMM'
+                    }
+                });
+
+                moment.locale('uk', {
+                    calendar: {
+                        sameDay: 'HH:mm',
+                        lastDay: 'D MMM',
+                        nextDay: '[завтра]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'D MMM',
+                        sameElse: 'D MMM'
+                    }
+                });
+
+                moment.locale($translate.use());
+
+                return;
+            }
+
+            if (vm.isSendTime) {
+                moment.locale('ru', {
+                    calendar: {
+                        sameDay: '[сегодня в] HH:mm',
+                        lastDay: '[вчера] hh:mm',
+                        nextDay: '[завтра]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'DD MMMM YYYY [в] hh:mm',
+                        sameElse: 'DD MMMM YYYY [в] hh:mm'
+                    }
+                });
+
+                moment.locale('en', {
+                    calendar: {
+                        sameDay: '[today] HH:mm',
+                        lastDay: '[yesterday] hh:mm',
+                        nextDay: '[tomorrow at]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'DD MMMM YYYY [at] hh:mm',
+                        sameElse: 'DD MMMM YYYY [at] hh:mm'
+                    }
+                });
+
+                moment.locale('cs', {
+                    calendar: {
+                        sameDay: '[dnes] HH:mm',
+                        lastDay: '[včera] hh:mm',
+                        nextDay: '[zítra]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'DD MMMM YYYY [v] hh:mm',
+                        sameElse: 'DD MMMM YYYY [v] hh:mm'
+                    }
+                });
+
+                moment.locale('sk', {
+                    calendar: {
+                        sameDay: '[dnes] HH:mm',
+                        lastDay: '[včera] hh:mm',
+                        nextDay: '[zítra]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'DD MMMM YYYY [v] hh:mm',
+                        sameElse: 'DD MMMM YYYY [v] hh:mm'
+                    }
+                });
+
+                moment.locale('uk', {
+                    calendar: {
+                        sameDay: '[сьогодні] HH:mm',
+                        lastDay: '[вчора] hh:mm',
+                        nextDay: '[завтра]',
+                        nextWeek: 'dddd HH:mm',
+                        lastWeek: 'DD MMMM YYYY [в] hh:mm',
+                        sameElse: 'DD MMMM YYYY [в] hh:mm'
+                    }
+                });
+
+                moment.locale($translate.use());
+
+                return
+            }
+
+            moment.locale('ru', {
+                calendar: {
+                    sameDay: 'HH:mm',
+                    lastDay: '[вчера] hh:mm',
+                    nextDay: '[завтра]',
+                    nextWeek: 'dddd HH:mm',
+                    lastWeek: 'DD MMMM YYYY [в] hh:mm',
+                    sameElse: 'DD MMMM YYYY [в] hh:mm'
+                }
+            });
+
+            moment.locale('en', {
+                calendar: {
+                    sameDay: 'HH:mm',
+                    lastDay: '[yesterday] hh:mm',
+                    nextDay: '[tomorrow at]',
+                    nextWeek: 'dddd HH:mm',
+                    lastWeek: 'DD MMMM YYYY [at] hh:mm',
+                    sameElse: 'DD MMMM YYYY [at] hh:mm'
+                }
+            });
+
+            moment.locale('cs', {
+                calendar: {
+                    sameDay: 'HH:mm',
+                    lastDay: '[včera] hh:mm',
+                    nextDay: '[zítra]',
+                    nextWeek: 'dddd HH:mm',
+                    lastWeek: 'DD MMMM YYYY [v] hh:mm',
+                    sameElse: 'DD MMMM YYYY [v] hh:mm'
+                }
+            });
+
+            moment.locale('sk', {
+                calendar: {
+                    sameDay: 'HH:mm',
+                    lastDay: '[včera] hh:mm',
+                    nextDay: '[zítra]',
+                    nextWeek: 'dddd HH:mm',
+                    lastWeek: 'DD MMMM YYYY [v] hh:mm',
+                    sameElse: 'DD MMMM YYYY [v] hh:mm'
+                }
+            });
+
+            moment.locale('uk', {
+                calendar: {
+                    sameDay: 'HH:mm',
+                    lastDay: '[вчора] hh:mm',
+                    nextDay: '[завтра]',
+                    nextWeek: 'dddd HH:mm',
+                    lastWeek: 'DD MMMM YYYY [в] hh:mm',
+                    sameElse: 'DD MMMM YYYY [в] hh:mm'
+                }
+            });
+
+            moment.locale($translate.use());
         }
 
         function getConvert(date) {
-            if (vm.isSmall) {
-                return moment(date).calendar(null, vm.calendarSmallFormat);
-            }
-            return moment(date).calendar(null, vm.calendarFormat);
+            return moment(date).calendar();
         }
 
         function getUnixConvert(date) {
-            if (vm.isSmall) {
-                return moment.unix(date).calendar(null, vm.calendarSmallFormat);
-            }
-            return moment.unix(date).calendar(null, vm.calendarFormat);
+            return moment.unix(date).calendar();
         }
     }
 })();
