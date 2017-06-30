058ef7419410ad5e7f9aaca9488d420169b89075
Aleksand Semenov <lovealldevelop@gmail.com>
2017-06-30 17:04:20 +0300

    Added build

--- src/client/app/components/attach-item/attach-item.controller.js
+++ src/client/app/components/attach-item/attach-item.controller.js
@@ -5,15 +5,16 @@
         .module('app.components')
         .controller('AttachItemController', AttachItemController);
 
-    AttachItemController.$inject = ['$auth'];
+    AttachItemController.$inject = ['$auth', 'CONFIG'];
     /* @ngInject */
-    function AttachItemController($auth) {
+    function AttachItemController($auth, CONFIG) {
         var vm = this;
 
         activate();
 
         function activate() {
             vm.user = $auth.user;
+            vm.CONFIG = CONFIG;
 
             console.log('attach', vm.attach);
             console.log('message', vm.message);
