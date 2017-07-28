03710bdab3b1813ba8b1326338240215da44f645
Aleksand Semenov <lovealldevelop@gmail.com>
2017-07-28 19:35:29 +0300

    Added build

--- src/client/app/auth/password-reset/password-reset.controller.js
+++ src/client/app/auth/password-reset/password-reset.controller.js
@@ -24,9 +24,15 @@
 
         function requestPasswordReset(form) {
             if (form.$invalid) return;
-            // console.log(vm.userForm);
+
+            var data = vm.userForm.model;
+
+            if (data.username.split('@').length) {
+                data.username = data.username.split('@')[0];
+            }
+
             vm.userForm.isLoading = true;
-            $auth.requestPasswordReset(vm.userForm.model)
+            $auth.requestPasswordReset(data)
                 .then(function (response) {
                     vm.userForm.isLoading = false;
                     $state.go('passwordUpdate', {
@@ -36,7 +42,7 @@
                 .catch(function (response) {
                     // handle error response
                     vm.userForm.errors = response.data.data;
-                    console.log('error',vm.userForm.errors);
+                    console.log('error', vm.userForm.errors);
                 });
         }
 
