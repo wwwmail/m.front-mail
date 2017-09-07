7eb6b7458ffce3f48d0b1b1fd9a6f754f219c3b1
Aleksand Semenov <lovealldevelop@gmail.com>
2017-09-07 18:40:21 +0300

    Added avatar fix

--- src/client/app/components/avatar-name/avatar-name.controller.js
+++ src/client/app/components/avatar-name/avatar-name.controller.js
@@ -31,12 +31,12 @@
 
         function getNames() {
             if (vm.name) {
-                var name = vm.name.replace(/[()"]/g, '');
+                var name = vm.name.replace(/[^A-Za-zА-Яа-яЁё0-9]/g, "");
                 var firstLetter = name.split(' ')[0].slice(0, 1);
-                vm.firstLetter = firstLetter.replace(/ /g, '').replace(/[()"]/g, '');
+                vm.firstLetter = firstLetter.replace(/ /g, '').replace(/[^A-Za-zА-Яа-яЁё0-9]/g, "");
                 if (vm.name.split(' ')[1]) {
                     var lastLetter = vm.name.split(' ')[1].slice(0, 1);
-                    vm.lastLetter = lastLetter.replace(/ /g, '').replace(/[()"]/g, '');
+                    vm.lastLetter = lastLetter.replace(/ /g, '').replace(/[^A-Za-zА-Яа-яЁё0-9]/g, "");
                 }
                 return;
             }
