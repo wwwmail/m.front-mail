03f0f7405b10097a952446bca6225510846a2e84
Aleksand Semenov <lovealldevelop@gmail.com>
2017-11-22 13:32:46 +0200

    Added build

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -3,11 +3,12 @@
 
     window.appConfig = {
         env: 'sandbox',
-        domainZone: 'cz',
-        parentHost: 'https://mail.cz',
-        APIHost: 'http://apimail.devogic.com',
-        MediaUrl: 'http://apimail.devogic.com/',
-        AttachUrl: 'http://apimail.devogic.com/mail/',
+        domainZone: 'group',
+        parentHost: 'https://wwwmailcz.dev.mail.group',
+        APIHost: 'https://mailcz.dev.mail.group',
+        MediaUrl: 'https://mailcz.dev.mail.group/',
+        AttachUrl: 'https://mailcz.dev.mail.group/mail/',
+        passportLink: 'https://passport.wwwmailcz.dev.mail.group',
         payment: {
             sandbox: 'AQgVJbIVhUkthElCdiLS1GZj7tq38wDeBspxYrLG_K73OzXL2_NTvBsQTDT9oXhEMTGQIiEtr8jPnFzF',
             production: 'AVZhosFzrnZ5Mf3tiOxAD0M6NHv8pcB2IFNHAfp_h69mmbd-LElFYkJUSII3Y0FPbm7S7lxBuqWImLbl'
