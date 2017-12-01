ca6020169a600277b16f6d03670d5824cc3827d7
Aleksand Semenov <lovealldevelop@gmail.com>
2017-12-01 19:28:11 +0200

    Added build

--- src/client/app/components/attach-item/attach-item.controller.js
+++ src/client/app/components/attach-item/attach-item.controller.js
@@ -91,7 +91,7 @@
 
         vm.fileFormat = null;
 
-        vm.viewAppUrl = 'http://docs.google.com/viewer?url=';
+        vm.viewAppUrl = 'https://view.officeapps.live.com/op/view.aspx?src=';
 
         vm.openAttach = openAttach;
 
