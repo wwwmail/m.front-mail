e70735b056478510f24a6c10b164ee227b53a931
Aleksand Semenov <lovealldevelop@gmail.com>
2017-12-07 16:48:22 +0200

    Stage commit

--- src/client/app/marketing/home/home.controller.js
+++ src/client/app/marketing/home/home.controller.js
@@ -5,13 +5,46 @@
         .module('marketing.home')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = [];
+    HomeController.$inject = ['$auth', '$state', '$stateParams', 'profile'];
     /* @ngInject */
-    function HomeController() {
+    function HomeController($auth, $state, $stateParams, profile) {
         var vm = this;
 
         activate();
 
-        function activate() {}
+        ////
+
+        function activate() {
+            console.log('profile', profile);
+
+            var params = {};
+
+            if ($stateParams.compose) {
+                params.compose = $state.params.compose
+            }
+
+            if ($stateParams.page) {
+                $state.go($stateParams.page, params);
+                return;
+            }
+
+            if ($stateParams.token) {
+                $auth.setAuthHeaders({
+                    "Authorization": 'Bearer ' + $stateParams.token
+                });
+
+                $auth.validateUser().then(function (response) {
+                    profile.addStorageProfile(response);
+                });
+
+                $state.go('mail.inbox', {mbox: 'INBOX'});
+
+                return;
+            }
+
+            params.mbox = 'INBOX';
+
+            $state.go('mail.inbox', params);
+        }
     }
 })();
--- src/client/app/marketing/home/home.route.js
+++ src/client/app/marketing/home/home.route.js
@@ -18,39 +18,7 @@
                 config: {
                     url: '/?version&token&page&compose&success',
                     controller: 'HomeController',
-                    controllerAs: 'vm',
-                    onEnter: function ($auth, $state, $stateParams, $rootScope, profile) {
-                        console.log('profile', profile);
-
-                        var params = {};
-
-                        if ($stateParams.compose) {
-                            params.compose = $state.params.compose
-                        }
-
-                        if ($stateParams.page) {
-                            $state.go($stateParams.page, params);
-                            return;
-                        }
-
-                        if ($stateParams.token) {
-                            $auth.setAuthHeaders({
-                                "Authorization": 'Bearer ' + $stateParams.token
-                            });
-
-                            $auth.validateUser().then(function (response) {
-                                profile.addStorageProfile(response);
-                            });
-
-                            $state.go('mail.inbox', {mbox: 'INBOX'});
-
-                            return;
-                        }
-
-                        params.mbox = 'INBOX';
-
-                        $state.go('mail.inbox', params);
-                    }
+                    controllerAs: 'vm'
                 }
             }
         ];
